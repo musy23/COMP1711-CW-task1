687d6b862d6c87e8c1c2d98744d69a71f2b5cc8a
Mustafa Syed <146679440+musy23@users.noreply.github.com>
2023-11-02 13:22:02 +0000

    Figured out how to print first three lines from file.

---
+++ stepstask1_practice.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Define an appropriate struct
+typedef struct {
+	char date[11];
+	char time[6];
+	int steps;
+} FITNESS_DATA;
+
+// Define any additional variables here
+
+// This is your helper function. Do not change it in any way.
+
+// Inputs: character array representing a row; the delimiter character
+// Ouputs: date character array; time character array; steps character array
+void tokeniseRecord(const char *input, const char *delimiter,
+                    char *date, char *time, char *steps) {
+    // Create a copy of the input string as strtok modifies the string
+    char *inputCopy = strdup(input);
+    
+    // Tokenize the copied string
+    char *token = strtok(inputCopy, delimiter);
+    if (token != NULL) {        strcpy(date, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(time, token);
+    }
+    
+    token = strtok(NULL, delimiter);
+    if (token != NULL) {
+        strcpy(steps, token);
+    }
+    
+    // Free the duplicated string
+    free(inputCopy);
+
+}
+
+// Complete the main function
+int main() {
+    int buffer_size = 10000;
+    char line_buffer[buffer_size];
+    int linestoprint=3;
+    int linesprinted=0;
+    //Used the example shown in class for following code:
+    char filename [] = "FitnessData_2023.csv";
+    FILE *file = fopen("FitnessData_2023.csv", "r");
+    if (file == NULL) {
+        perror("");
+        return 1;
+    }
+    //printed first three lines from .csv file
+    while (fgets(line_buffer, sizeof(line_buffer), file) != NULL && linesprinted < linestoprint){
+        printf("%s", line_buffer);
+        linesprinted++;
+    }
+
+    fclose(file);
+    return 0;
+    
+}
